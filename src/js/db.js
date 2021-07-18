6408b9bfe91e635cf99e5c29bcc4e2eac1da54de
Space Codee <spacecodee@gmail.com>
2021-07-17 22:52:13 -0500

    Modificated

--- src/js/db.js
+++ src/js/db.js
@@ -1,8 +1,8 @@
 const mysql = require('promise-mysql');
 const connection = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    password: 'RONNYharol07',
+    user: 'scodee',
+    password: 'spacecodee',
     database: 'chatapp'
 });
 
